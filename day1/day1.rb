813787901576e760cc238815b13f7ceb8ae99fb7
Shawn Veader <shawn@veader.org>
2015-12-16 15:41:51 -0500

    Day1: star 2

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
 
 def move_elevator(input)
-  0 + input.count('(') - input.count(')')
+  # 0 + input.count('(') - input.count(')')
+  floor = 0
+  basement_position = nil
+  input.chars.each_with_index do |char, idx|
+    case char
+    when "("
+      floor += 1
+    when ")"
+      floor -= 1
+      basement_position = idx + 1 if basement_position.nil? && floor < 0
+    end
+  end
+  [floor, moved_to_basement_position]
 end
 
 if __FILE__ == $0
   input = ARGV[0].gsub(/[^()]/,'')
-  floor = move_elevator(input)
-  puts "Final floor: #{floor}"
+  floor, basement = move_elevator(input)
+  puts "Final floor: #{floor}, moved to basement on #{basement}"
 end
