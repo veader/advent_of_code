8538820c53e73f5c0bf2de82e6c182516badb22b
Shawn Veader <shawn@veader.org>
2015-12-23 23:05:53 -0500

    Day 12: **

--- day12/day12.rb
+++ day12/day12.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
-# require "json"
+require "json"
 require "pp"
 
 class AddDoc
   attr_accessor :json_doc, :string_doc
 
   def initialize(string_doc="{}")
+    @found_numbers = []
+
     self.string_doc = string_doc
+    parse_json!
+    find_numbers(self.json_doc)
   end
 
   def hack_add
@@ -17,13 +21,43 @@ class AddDoc
     numbers.reduce(:+) || 0
   end
 
-  # def parse_json!
-  #   self.json_doc = JSON.parse(self.string_doc)
-  # end
+  def parse_json!
+    self.json_doc = JSON.parse(self.string_doc)
+  end
+
+  def find_numbers(element)
+    # walk the JSON element and find numbers
+    if element.is_a?(Hash) # ---- Object
+      return if element.values.include?("red")
+      element.values.each do |val|
+        if val.is_a?(Fixnum)
+          @found_numbers << val
+        else
+          find_numbers(val)
+        end
+      end
+    elsif element.is_a?(Array) # ---- Array
+      element.each do |item|
+        if item.is_a?(Fixnum)
+          @found_numbers << item
+        else
+          find_numbers(item)
+        end
+      end
+    else
+      p "What is this? '#{element}'"
+    end
+  end
+
+  def sum
+    p "Found: #{@found_numbers}"
+    @found_numbers.reduce(:+) || 0
+  end
 end
 
 if __FILE__ == $0
   input = ARGV[0]
   doc = AddDoc.new(input)
   p "Sum: (HACK) #{doc.hack_add}"
+  p "Sum: #{doc.sum}"
 end
--- day12/day12_test.rb
+++ day12/day12_test.rb
@@ -10,10 +10,14 @@ require_relative "day12"
   ['[-1,{"a":1}]', 0],
   ['[]', 0],
   ['{}', 0],
+  ['[1,{"c":"red","b":2},3]', 4],
+  ['{"d":"red","e":[1,2,3,4],"f":5}', 0],
+  ['[1,"red",5]', 6],
 ].each do |test_data|
   ans = test_data.last
   pp str = test_data.first
   doc = AddDoc.new(str)
-  sum = doc.hack_add
+  # sum = doc.hack_add
+  sum = doc.sum
   raise "Sum should equal #{ans} for #{str} - got #{sum}" unless sum == ans
 end
