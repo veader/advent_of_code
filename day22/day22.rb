9609fc19aa29b89459804aea461b002d37dccb0b
Shawn Veader <shawn@veader.org>
2016-01-11 12:20:17 -0500

    Day 22: WIP - working game mechanics

---
+++ day22/day22.rb
@@ -0,0 +1,273 @@
+#!/usr/bin/env ruby
+
+require "pp"
+
+DEBUG=true
+
+class Spell
+  attr_accessor :name, :cost, :instant_damage, :instant_healing
+  attr_accessor :repeating_damage, :repeating_armor, :repeating_mana
+  attr_accessor :timer, :repeats
+
+  def initialize(name, cost=0, damage=0, healing=0, repeats=0, repeat_damage=0, repeat_armor=0, repeat_mana=0)
+    self.name = name
+    self.cost = cost
+
+    # instantanious damage and healing when spell is cast
+    self.instant_damage = damage
+    self.instant_healing = healing
+
+    # for repeating spells
+    self.repeating_damage = repeat_damage
+    self.repeating_armor = repeat_armor
+    self.repeating_mana = repeat_mana
+
+    # how many times the spell repeats and the active timer (once spell is cast)
+    self.repeats = repeats
+    self.timer = 0
+  end
+
+  def effect_type
+    if self.repeating_damage > 0
+      :damage
+    elsif self.repeating_armor > 0
+      :armor
+    elsif self.repeating_mana > 0
+      :mana
+    else
+      nil
+    end
+  end
+
+  def cast!(attacker, defender)
+    p "#{attacker.name.capitalize} casts #{self.name}." if DEBUG
+    defender.defend!(self.instant_damage)
+    attacker.heal!(self.instant_healing)
+    attacker.use_mana(self.cost)
+
+    if self.repeats?
+      spell_dupe = self.dup # duplicate us so we don't overwrite things
+      spell_dupe.timer = self.repeats
+      attacker.add_active_spell(spell_dupe)
+      # spell_dupe.repeat_cast!(attacker, defender) # go ahead and take first turn
+    end
+  end
+
+  def repeat_cast!(attacker, defender)
+    if self.repeating_damage > 0
+      p "#{self.name} deals #{self.repeating_damage}; its timer is now #{self.timer-1}" if DEBUG
+      defender.defend!(self.repeating_damage)
+    end
+    if self.repeating_armor  > 0
+      p "#{self.name} provides #{self.repeating_armor} armor; its timer is now #{self.timer-1}" if DEBUG
+      attacker.add_armor!(self.repeating_armor)
+    end
+    if self.repeating_mana   > 0
+      p "#{self.name} provides #{self.repeating_mana} mana; its timer is now #{self.timer-1}" if DEBUG
+      attacker.add_mana!(self.repeating_mana)
+    end
+    self.timer -= 1 # use a turn
+  end
+
+  def repeats?
+    self.repeats > 0
+  end
+
+  def complete?
+    self.timer <= 0
+  end
+
+  def print
+    output = name
+    output += " costs #{self.cost}. "
+    if self.instant_damage > 0 || self.instant_healing > 0
+      output += "It instantly"
+      output += " does #{self.instant_damage} damage" if self.instant_damage > 0
+      output += " and" if self.instant_damage > 0 && self.instant_healing > 0
+      output += " heals you for #{self.instant_healing} hit points" if self.instant_healing > 0
+      output += ". "
+    end
+    if self.repeats?
+      output += "It starts an effect that lasts for #{self.repeats} turns. "
+      output += "While it is active, your armor is increased by #{self.repeating_armor}." if self.repeating_armor > 0
+      output += "At the start of each turn while it is active, it deals the boss #{self.repeating_damage} damage." if self.repeating_damage > 0
+      output += "At the start of each turn while it is active, it gives you #{self.repeating_mana} new mana." if self.repeating_mana > 0
+    end
+    p output if DEBUG
+  end
+end
+
+class Player
+  attr_accessor :name, :hitpoints, :original_hitpoints, :mana, :original_mana
+  attr_accessor :base_damage, :base_armor, :armor
+  attr_accessor :spells, :active_spells
+
+  def initialize(name, hp, mana, damage=0)
+    self.name = name
+    self.hitpoints = hp
+    self.original_hitpoints = hp
+    self.mana = mana
+    self.original_mana = mana
+    self.base_damage = damage
+    self.base_armor = 0
+    self.armor = 0
+
+    self.active_spells = []
+
+    setup_spells
+  end
+
+  def setup_spells
+    self.spells = \
+    [
+      ["Magic Missile", 53,   4, 0, 0, 0, 0, 0],
+      ["Drain",         73,   2, 2, 0, 0, 0, 0],
+      ["Shield",        113,  0, 0, 6, 0, 7, 0],
+      ["Poison",        173,  0, 0, 6, 3, 0, 0],
+      ["Recharge",      229,  0, 0, 5, 0, 0, 101],
+    ].collect do |spell_array|
+      Spell.new(*spell_array)
+    end
+  end
+
+  def print_spells
+    self.spells.each do |spell|
+      spell.print
+      p "-"*10
+    end
+  end
+
+  def print_status
+    p "- #{self.name.capitalize} has #{self.hitpoints} hit points" + \
+      ", #{self.armor} armor, #{self.mana} mana" if DEBUG
+  end
+
+  def find_spell(name)
+    self.spells.find { |s| s.name == name }
+  end
+
+  # to cast a spell, we have to have enough mana and the spell can't have
+  #     the same effect as an active spell
+  def can_cast_spell?(name)
+    return false unless spell = find_spell(name)
+    spell.cost <= self.mana && !active_effects_collide?(spell)
+  end
+
+  def active_effects_collide?(spell)
+    return false unless effect = spell.effect_type
+    !(self.active_spells.detect { |s| s.effect_type == effect }).nil?
+  end
+
+  def attack!(player, spell=nil)
+    handle_active_spells(player)
+    return if self.dead_yet? || player.dead_yet?
+
+    if self.base_damage > 0
+      p "#{self.name.capitalize} attacks for #{self.base_damage} - #{player.armor} damage" if DEBUG
+      player.defend!(self.base_damage)
+    end
+    return if self.dead_yet? || player.dead_yet?
+
+    if spell && can_cast_spell?(spell.name)
+      spell.cast!(self, player)
+    end
+  end
+
+  def defend!(incoming_damage)
+    self.hitpoints -= (incoming_damage - self.armor)
+  end
+
+  def heal!(added_hitpoints)
+    self.hitpoints += added_hitpoints
+  end
+
+  def add_armor!(additional_armor)
+    self.armor = self.base_armor + additional_armor
+  end
+
+  def add_mana!(additional_mana)
+    self.mana += additional_mana
+  end
+
+  def use_mana(spell_cost)
+    self.mana -= spell_cost
+  end
+
+  def add_active_spell(spell)
+    self.active_spells << spell
+  end
+
+  def remove_active_spell(spell)
+    self.active_spells.delete(spell)
+  end
+
+  def handle_active_spells(player)
+    spells_to_remove = []
+    self.active_spells.each do |spell|
+      spell.repeat_cast!(self, player)
+      spells_to_remove << spell if spell.complete?
+    end
+    spells_to_remove.each do |spell|
+      p "#{spell.name} wears off" if DEBUG
+      remove_active_spell(spell)
+    end
+  end
+
+  def dead_yet?
+    # yes if we are out of hit points or mana
+    self.hitpoints <= 0
+  end
+
+  def reset!
+    self.hitpoints = self.original_hitpoints
+    self.mana = self.original_mana
+    self.armor = self.base_armor
+    self.active_spells = []
+  end
+end
+
+class Game
+  attr_accessor :player, :boss
+
+  def initialize
+    self.player = Player.new("player", 50, 500) # use defaults
+    self.boss   = Player.new("boss", 71, 0, 10) # from puzzle input
+  end
+
+  # returns winner
+  def play!(spells_to_cast=[])
+    attacker = self.boss
+    defender = self.player
+
+    attacker.reset!
+    defender.reset!
+
+    until defender.dead_yet?
+      attacker, defender = defender, attacker # switch roles
+      p "-- #{attacker.name.capitalize} turn --" if DEBUG
+      self.player.print_status
+      self.boss.print_status
+
+      # pop a spell off the stack for the player each time
+      spell = nil
+      if attacker == self.player && self.player.can_cast_spell?(spells_to_cast.first)
+        spell = self.player.find_spell(spells_to_cast.shift)
+      end
+      defender.handle_active_spells(attacker)
+      attacker.attack!(defender, spell)
+      p ""
+    end
+
+    if DEBUG
+      p "#{attacker.name.capitalize} wins! (and still has #{attacker.hitpoints} HP)"
+    else
+      print "."
+    end
+    attacker
+  end
+end
+
+# ============================================================================
+if __FILE__ == $0
+  game = Game.new
+end
---
+++ day22/day22_test.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require "pp"
+require_relative "day22"
+
+player = Player.new("test", 100, 0)
+
+spell_name = "Poison"
+raise "Should not be able to cast spell with 0 mana" if player.can_cast_spell?(spell_name)
+player.mana = 200
+raise "Should be able to cast spell." unless player.can_cast_spell?(spell_name)
+spell = player.find_spell(spell_name)
+player.active_spells = [spell]
+raise "Should not be able to stack damage effect" if player.can_cast_spell?(spell_name)
+
+spell_name = "Sheild"
+spell = player.find_spell(spell_name)
+player.active_spells = [spell]
+raise "Should not be able to stack armor effect" if player.can_cast_spell?(spell_name)
+
+spell_name = "Recharge"
+spell = player.find_spell(spell_name)
+player.active_spells = [spell]
+raise "Should not be able to stack mana effect" if player.can_cast_spell?(spell_name)
+
+
+game = Game.new
+game.player.original_hitpoints = 10
+game.player.original_mana = 250
+game.boss.original_hitpoints = 13
+game.boss.base_damage = 8
+## EXAMPLE 1
+# game.play!(["Poison", "Magic Missile"])
+
+## EXAMPLE 2
+game.boss.original_hitpoints = 14
+game.play!(["Recharge", "Shield", "Drain", "Poison", "Magic Missile"])
