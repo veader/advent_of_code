0de407361546c381db3d6761cf5a20606935a6c2
Shawn Veader <shawn@veader.org>
2016-12-25 23:03:45 -0500

    Day 14: **

--- 2016/day14/solution.rb
+++ 2016/day14/solution.rb
@@ -4,6 +4,7 @@ require 'digest/md5'
 
 MD5_HASHES = []
 SALT = 'ahsbgdzn'
+STRETCH_REPEAT = 1
 
 found_keys = []
 index = 1
@@ -15,13 +16,21 @@ def make_key(idx)
     (SALT + idx.to_s).downcase
 end
 
+def make_hash(key, repeat=STRETCH_REPEAT)
+    output = key
+    repeat.times do |i|
+        output = Digest::MD5.hexdigest(output).downcase
+    end
+    return output
+end
+
 def is_valid_key?(idx, char)
     idx += 1 # move past our current index
 
     1000.times do |offset|
         new_index = idx + offset
         key = make_key(new_index)
-        md5 = MD5_HASHES[new_index] || Digest::MD5.hexdigest(key)
+        md5 = MD5_HASHES[new_index] || make_hash(key)
         MD5_HASHES[new_index] = md5 if MD5_HASHES[new_index].nil?
 
         return true if match = /#{char}{5}/.match(md5)
@@ -32,7 +41,7 @@ end
 
 while found_keys.count < 64 do
     key = make_key(index)
-    md5 = MD5_HASHES[index] || Digest::MD5.hexdigest(key)
+    md5 = MD5_HASHES[index] || make_hash(key)
     MD5_HASHES[index] = md5 if MD5_HASHES[index].nil?
 
     if match = triple_regex.match(md5) then
