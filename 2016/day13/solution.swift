baf17defe2bb8b99c9f891f6bc72a9d5cf3eb04d
Shawn Veader <shawn@veader.org>
2017-01-04 00:41:03 -0500

    Day 13: ** WIP - closer

--- 2016/day13/solution.swift
+++ 2016/day13/solution.swift
@@ -9,6 +9,7 @@ struct Map {
     let favorite: Int
 
     var coordinates: [[Coordinate]]
+    var solution: [Coordinate]
 
     init(w theWidth: Int, h theHeight: Int, favorite fav: Int) {
         width = theWidth
@@ -16,6 +17,7 @@ struct Map {
         favorite = fav
 
         coordinates = [[Coordinate]]()
+        solution = [Coordinate]()
 
         (0..<width).forEach { x in
             let column = (0..<height).map { y in
@@ -36,6 +38,89 @@ struct Map {
     func spot(at: (x: Int, y: Int)) -> Coordinate {
         return coordinates[at.x][at.y]
     }
+
+    func open(from: Coordinate) -> [Coordinate] {
+        var available = [Coordinate]()
+
+        // cheating a bit and trying to go down and right first
+        if from.y + 1 < height {
+            let downSpot = spot(at: (from.x, from.y + 1))
+            if !downSpot.isWall() {
+                available.append(downSpot)
+            }
+        }
+        if from.x + 1 < width {
+            let rightSpot = spot(at: (from.x + 1, from.y))
+            if !rightSpot.isWall() {
+                available.append(rightSpot)
+            }
+        }
+        if from.x - 1 >= 0 {
+            let leftSpot = spot(at: (from.x - 1, from.y))
+            if !leftSpot.isWall() {
+                available.append(leftSpot)
+            }
+        }
+        if from.y - 1 >= 0 {
+            let upSpot = spot(at: (from.x, from.y - 1))
+            if !upSpot.isWall() {
+                available.append(upSpot)
+            }
+        }
+
+        let coords = available.map { "\($0.x),\($0.y)" }
+        print("Openings from \(from.x),\(from.y): \(coords)")
+        return available
+    }
+
+    func pathAsText(_ path: [Coordinate]) -> String {
+        guard path.count > 0 else { return "NONE" }
+        return path.map { "\($0.x),\($0.y)" }.joined(separator: " -> ")
+    }
+
+    mutating func navigate(to destination: (x: Int, y: Int), path current: [Coordinate] = [Coordinate]()) {
+        if solution.count > 0 && current.count > solution.count {
+            return
+        }
+
+        let currentCoordinate: Coordinate
+        if let theCurrentCoordinate = current.last {
+            currentCoordinate = theCurrentCoordinate
+        } else {
+            let startingCoordinate = spot(at: (1, 1)) // starting location
+            navigate(to: destination, path: [startingCoordinate])
+            return
+        }
+
+        // have we made it to the destination?
+        if currentCoordinate.x == destination.x && currentCoordinate.y == destination.y {
+            print(pathAsText(current))
+            print("Current: \(current.count) Existing: \(solution.count)")
+            if solution.count == 0 || current.count < solution.count {
+                solution = current
+            } else {
+                print("Alternate Solution: \(current)")
+            }
+            return
+        }
+
+        let openCoordinates = open(from: currentCoordinate)
+        if openCoordinates.count > 0 {
+            var previousCoordinate: Coordinate?
+            if current.count > 1 {
+                let previousIndex = current.index(current.endIndex, offsetBy: -2)
+                previousCoordinate = current[previousIndex]
+            }
+
+            for coord in openCoordinates {
+                if coord != previousCoordinate { // make sure we're not going backward
+                    var newPath: [Coordinate] = current
+                    newPath.append(coord)
+                    navigate(to: destination, path: newPath)
+                }
+            }
+        }
+    }
 }
 
 extension Map: CustomDebugStringConvertible {
@@ -46,7 +131,13 @@ extension Map: CustomDebugStringConvertible {
         gridDisplay.append("  \(xIndex)\n")
 
         (0..<height).forEach { y in
-            let rowChars = row(y).map { $0.debugDescription }.joined()
+            let rowChars = row(y).map { c in
+                if solution.contains(c) {
+                    return "O"
+                } else {
+                    return c.debugDescription
+                }
+            }.joined()
             gridDisplay.append("\(String(y % 10)) \(rowChars)")
             gridDisplay.append("\n")
         }
@@ -76,16 +167,23 @@ struct Coordinate {
     }
 }
 
+extension Coordinate: Equatable {}
+func ==(lhs: Coordinate, rhs: Coordinate) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
 extension Coordinate: CustomDebugStringConvertible {
     var debugDescription: String {
-        let wall = isWall() ? "#" : "."
-        // return "X: \(x) Y: \(y) - \(wall)"
-        return wall
+        return isWall() ? "#" : "."
     }
 }
 
 // ----------------------------------------------------------------------------
-let favorite = 1350
+let favorite = 1350  // 10
+let width  = 40 // 10
+let height = 40 // 10
 
-let map = Map.init(w: 40, h: 40, favorite: favorite)
+var map = Map.init(w: width, h: height, favorite: favorite)
 print(map)
+// map.navigate(to: (31, 39)) // 7,4
+// print(map)
