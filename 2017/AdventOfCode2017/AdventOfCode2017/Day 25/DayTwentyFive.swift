a7ba8ef27822e2caccc9b02f32a5efb081a6723d
Shawn Veader <shawn@veader.org>
2018-01-08 08:51:20 -0500

    Day 25: Init

---
+++ 2017/AdventOfCode2017/AdventOfCode2017/Day 25/DayTwentyFive.swift
@@ -0,0 +1,9 @@
+//
+//  DayTwentyFive.swift
+//  AdventOfCode2017
+//
+//  Created by Shawn Veader on 1/7/18.
+//  Copyright © 2018 v8logic. All rights reserved.
+//
+
+import Foundation
---
+++ 2017/AdventOfCode2017/AdventOfCode2017/Tests/DayTwentyFiveTests.swift
@@ -0,0 +1,9 @@
+//
+//  DayTwentyFiveTests.swift
+//  AdventOfCode2017
+//
+//  Created by Shawn Veader on 1/7/18.
+//  Copyright © 2018 v8logic. All rights reserved.
+//
+
+import Foundation
