ea178beb8e9fae3c12c0166946fc44b75832fce2
Shawn Veader <shawn@veader.org>
2018-01-19 09:46:19 -0500

    Day 25: WIP

--- 2017/AdventOfCode2017/AdventOfCode2017/Day25/DayTwentyFive.swift
+++ 2017/AdventOfCode2017/AdventOfCode2017/Day25/DayTwentyFive.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+
+struct DayTwentyFive: AdventDay {
+
+    struct TuringMachine {
+        struct TuringMachineState {
+            let name: String
+        }
+
+        /// inifinte tape (of 0's to begin with)
+        let tape: [Int]
+
+        /// position along the tape
+        let cursor: Int = 0
+
+        /// current state
+        let state: TuringMachineState
+
+        /// available states
+        let states: [TuringMachineState]
+    }
+
+    // MARK: -
+
+    func defaultInput() -> String? {
+        let path = "\(dataPath())/day25input.txt"
+        return try? String(contentsOfFile: path, encoding: .utf8)
+    }
+
+    func run(_ input: String? = nil) {
+        guard let runInput = input ?? defaultInput() else {
+            print("Day 25: 💥 NO INPUT")
+            exit(10)
+        }
+
+        let thing = partOne(input: runInput)
+        guard let answer = thing else {
+            print("Day 25: (Part 1) 💥 Unable to calculate answer.")
+            exit(1)
+        }
+        print("Day 25: (Part 1) Answer ", answer)
+
+        // ...
+    }
+
+    // MARK: -
+
+    func partOne(input: String) -> Int? {
+        return nil
+    }
+
+    func partTwo(input: String) -> Int? {
+        return nil
+    }
+}
--- 2017/AdventOfCode2017/AdventOfCode2017/Tests/DayTwentyFiveTests.swift
+++ 2017/AdventOfCode2017/AdventOfCode2017/Tests/DayTwentyFiveTests.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+extension DayTwentyFive: Testable {
+    func runTests() {
+        let input = ""
+
+        guard
+            testValue(0, equals: partOne(input: input))
+            else {
+                print("Part 1 Tests Failed!")
+                return
+        }
+
+        guard
+            testValue(0, equals: partTwo(input: input))
+            else {
+                print("Part 2 Tests Failed!")
+                return
+        }
+
+        print("Done with tests... all pass")
+    }
+}
