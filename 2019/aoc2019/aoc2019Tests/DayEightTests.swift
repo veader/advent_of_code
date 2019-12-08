db0f05a527b8e0a455a7a92bebc66d9e6c4749d9
Shawn Veader <shawn@veader.org>
2019-12-08 10:09:27 -0500

    Day 8: **

--- 2019/aoc2019/aoc2019Tests/DayEightTests.swift
+++ 2019/aoc2019/aoc2019Tests/DayEightTests.swift
@@ -10,14 +10,55 @@ import XCTest
 
 class DayEightTests: XCTestCase {
     func testSpaceImage() {
-        let day = DayEight()
         var image: SpaceImage
         var data: [Int]
 
-        data = day.parse("123456789012")
+        data = DayEight().parse("123456789012")
         image = SpaceImage(width: 3, height: 2, data: data)
         XCTAssertEqual(2, image.layers.count)
         XCTAssertEqual(6, image.layers.first?.pixels.count)
+    }
+
+    func testLayerIndexingAndColor() {
+        let data = DayEight().parse("0222112222120000")
+        let image = SpaceImage(width: 2, height: 2, data: data)
+        XCTAssertEqual(4, image.layers.count)
+
+        var color: SpaceImage.Layer.PixelColor?
+
+        // make sure we can't color outside the bounds
+        XCTAssertNil(image.layers.first?.color(x: 0, y: 3))
+        XCTAssertNil(image.layers.first?.color(x: 3, y: 0))
+        XCTAssertNil(image.layers.first?.color(x: 3, y: 6))
+
+        color = image.layers.first?.color(x: 0, y: 0)
+        XCTAssertNotNil(color)
+        XCTAssertEqual(.black, color)
+
+        color = image.layers.first?.color(x: 1, y: 0)
+        XCTAssertNotNil(color)
+        XCTAssertEqual(.transparent, color)
+    }
+
+    func testImageColorAtPixel() {
+        let data = DayEight().parse("0222112222120000")
+        let image = SpaceImage(width: 2, height: 2, data: data)
+
+        var color: SpaceImage.Layer.PixelColor?
+
+        // make sure we can't color outside the bounds
+        XCTAssertNil(image.color(x: 0, y: 7))
+        XCTAssertNil(image.color(x: 7, y: 0))
+        XCTAssertNil(image.color(x: 7, y: 7))
+
+        color = image.color(x: 0, y: 0)
+        XCTAssertEqual(.black, color)
+        color = image.color(x: 1, y: 0)
+        XCTAssertEqual(.white, color)
+        color = image.color(x: 0, y: 1)
+        XCTAssertEqual(.white, color)
+        color = image.color(x: 1, y: 1)
+        XCTAssertEqual(.black, color)
     }
 
     func testPartOneAnswer() {
