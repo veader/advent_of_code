6128e8f366b8e3e21a6136906d7a5578080157b4
Shawn Veader <shawn@veader.org>
2019-12-17 11:35:17 -0500

    Day 16: ** (WIP)

--- 2019/aoc2019/aoc2019/day16/DaySixteen.swift
+++ 2019/aoc2019/aoc2019/day16/DaySixteen.swift
@@ -9,51 +9,96 @@
 import Foundation
 
 struct FFT {
-    var pattern: [Int] = [1, 0, -1, 0] // "base" pattern
+    static let pattern: [Int] = [1, 0, -1, 0] // "base" pattern
 
-    func process(input: [Int], phases: Int) -> [Int] {
+    @discardableResult
+    static func process(input: inout [Int], phases: Int, size: Int = 8, offset: Int = 0) -> [Int] {
+        var iterations = 0
+        while iterations < phases {
+            phase(input: &input, size: size, offset: offset)
+            iterations += 1
+        }
+
+        return input
+    }
+
+    @discardableResult
+    static func phase(input: inout [Int], size: Int = 8, offset: Int = 0) -> [Int] {
+        // edit the input in place (save memory allocations)
+        // let maxIdx = min(offset + size, input.count)
+        let maxIdx = input.count
+        print("Phase running over \(offset..<maxIdx)")
+        for idx in (offset..<maxIdx) {
+            input[idx] = phaseDigit(at: idx, stepSize: idx + 1, input: input)
+        }
+
+        return input
+    }
+
+    static private func phaseDigit(at offset: Int, stepSize: Int, input: [Int]) -> Int {
+        var patternIdx = 0
+        let numberOfSteps = input.count / stepSize
+
+        let output = (0..<numberOfSteps).reduce(0) { result, chunkIdx -> Int in
+            let idx = offset + (chunkIdx * stepSize)
+            let multiplier = pattern[patternIdx]
+            patternIdx = nextPatternIndex(patternIdx) // move to the next index for the next cycle
+            return result + sum(offset: idx, stepSize: stepSize, multiplier: multiplier, input: input)
+        }
+
+        return abs(output) % 10
+    }
+
+    static private func sum(offset: Int, stepSize: Int, multiplier: Int, input: [Int]) -> Int {
+        guard multiplier != 0 else { return 0 }
+        let maxIdx = min((offset + stepSize), input.count) // don't fall off the end
+
+        return input[offset..<maxIdx].reduce(0) { result, value -> Int in
+            return result + (value * multiplier)
+        }
+    }
+
+    static private func nextPatternIndex(_ idx: Int) -> Int {
+        (idx + 1) % pattern.count
+    }
+
+
+    // -------------------------
+
+    static func process1(input: [Int], phases: Int) -> [Int] {
         var iterations = 0
         var output: [Int] = input
 
         while iterations < phases {
-            output = phase(input: output)
+            output = phase1(input: output)
             iterations += 1
         }
 
         return output
     }
 
-
-    func phase(input: [Int]) -> [Int] {
-        let output = input.enumerated().map { tuple -> Int in
-            phase(digit: tuple.offset + 1, input: input)
+    static func phase1(input: [Int]) -> [Int] {
+        input.enumerated().map { tuple -> Int in
+            phase1(digit: tuple.offset + 1, input: input)
         }
-        // print(output)
-
-        return output
     }
 
-    private func phase(digit: Int, input: [Int]) -> Int {
-        // var outputText: [String] = [String]()
-        let repeatingPattern = pattern.flatMap { Array(repeating: $0, count: digit) }
-        // print(repeatingPattern)
+    static private func phase1(digit: Int, input: [Int]) -> Int {
+        let repeatingPattern = FFT.pattern.flatMap { Array(repeating: $0, count: digit) }
 
         let output = input.enumerated().map { (idx, int) -> Int in
             if idx + 1 < digit {
-                // outputText.append("\(int)*0")
                 return 0
             } else {
                 // calculate pattern multiplier
                 let multiplierIdx = max(0, idx - (digit - 1)) % repeatingPattern.count
                 let multiplier = repeatingPattern[multiplierIdx]
-                // outputText.append("\(int)*\(multiplier)[\(multiplierIdx)]")
                 return int * multiplier
             }
         }
-        let answer = output.reduce(0, +)
-        let answerText = "\(answer)".suffix(1)
-        // print(outputText.joined(separator: " + ") + " = \(answerText)")
-        return Int(answerText) ?? 0
+        var answer = output.reduce(0, +)
+        answer = abs(answer) % 10
+        return answer
     }
 }
 
@@ -68,13 +113,25 @@ struct DaySixteen: AdventDay {
 
     func partOne(input: String?) -> Any {
         print("Start: \(Date())")
-        let output = FFT().process(input: parse(input ?? ""), phases: 100)
+        let output = FFT.process1(input: parse(input ?? ""), phases: 100)
         print("End: \(Date())")
         let outputStr = output.prefix(8).map(String.init).joined()
         return outputStr
     }
 
     func partTwo(input: String?) -> Any {
-        return 0
+        print("Start: \(Date())")
+        let massInput = Array(repeating: input ?? "", count: 10_000).joined()
+        var signal = parse(massInput)
+        print("Signal size: \(signal.count)")
+        let offset = Int(String(signal.prefix(7).map(String.init).joined())) ?? 0
+        print("Offset: \(offset)")
+        print("Start Processing: \(Date())")
+        let output = FFT.process(input: &signal, phases: 100, size: 8, offset: offset)
+//        let offsetOutput = output.dropFirst(offset)
+//        let outputStr = offsetOutput.prefix(8).map(String.init).joined()
+        let outputStr = output[offset..<(offset+8)].compactMap(String.init).joined()
+        print("End: \(Date())")
+        return outputStr
     }
 }
--- 2019/aoc2019/aoc2019Tests/DaySixteenTests.swift
+++ 2019/aoc2019/aoc2019Tests/DaySixteenTests.swift
@@ -11,10 +11,8 @@ import XCTest
 class DaySixteenTests: XCTestCase {
 
     func testPhase() {
-        let fft = FFT()
-
         // first phase
-        var output = fft.phase(input: [1,2,3,4,5,6,7,8])
+        var output = FFT.phase1(input: [1,2,3,4,5,6,7,8])
         XCTAssertEqual(4, output[0])
         XCTAssertEqual(8, output[1])
         XCTAssertEqual(2, output[2])
@@ -25,7 +23,7 @@ class DaySixteenTests: XCTestCase {
         XCTAssertEqual(8, output[7])
 
         // second phase
-        output = fft.phase(input: output)
+        output = FFT.phase1(input: output)
         XCTAssertEqual(3, output[0])
         XCTAssertEqual(4, output[1])
         XCTAssertEqual(0, output[2])
@@ -36,7 +34,7 @@ class DaySixteenTests: XCTestCase {
         XCTAssertEqual(8, output[7])
 
         // third phase
-        output = fft.phase(input: output)
+        output = FFT.phase1(input: output)
         XCTAssertEqual(0, output[0])
         XCTAssertEqual(3, output[1])
         XCTAssertEqual(4, output[2])
@@ -47,7 +45,7 @@ class DaySixteenTests: XCTestCase {
         XCTAssertEqual(8, output[7])
 
         // fourth phase
-        output = fft.phase(input: output)
+        output = FFT.phase1(input: output)
         XCTAssertEqual(0, output[0])
         XCTAssertEqual(1, output[1])
         XCTAssertEqual(0, output[2])
@@ -59,7 +57,7 @@ class DaySixteenTests: XCTestCase {
     }
 
     func testFFTProcess() {
-        let output = FFT().process(input: [1,2,3,4,5,6,7,8], phases: 4)
+        let output = FFT.process1(input: [1,2,3,4,5,6,7,8], phases: 4)
         XCTAssertEqual(0, output[0])
         XCTAssertEqual(1, output[1])
         XCTAssertEqual(0, output[2])
@@ -72,26 +70,73 @@ class DaySixteenTests: XCTestCase {
 
     func testPartOne() {
         let day = DaySixteen()
-        let fft = FFT()
 
         var input: [Int]
         var output: [Int]
         var outputStr: String
 
         input = day.parse("80871224585914546619083218645595")
-        output = fft.process(input: input, phases: 100)
+        output = FFT.process1(input: input, phases: 100)
         outputStr = output.prefix(8).map(String.init).joined()
         XCTAssertEqual("24176176", outputStr)
 
         input = day.parse("19617804207202209144916044189917")
-        output = fft.process(input: input, phases: 100)
+        output = FFT.process1(input: input, phases: 100)
         outputStr = output.prefix(8).map(String.init).joined()
         XCTAssertEqual("73745418", outputStr)
 
         input = day.parse("69317163492948606335995924319873")
-        output = fft.process(input: input, phases: 100)
+        output = FFT.process1(input: input, phases: 100)
         outputStr = output.prefix(8).map(String.init).joined()
         XCTAssertEqual("52432133", outputStr)
+    }
+
+    func testPhase2Idea() {
+        let day = DaySixteen()
+
+        var input: [Int]
+        var output: [Int]
+        var outputStr: String
+
+        input = day.parse("80871224585914546619083218645595")
+        output = FFT.process(input: &input, phases: 100)
+        outputStr = output.prefix(8).map(String.init).joined()
+        XCTAssertEqual("24176176", outputStr)
+
+//        input = day.parse("19617804207202209144916044189917")
+//        output = FFT.process(input: &input, phases: 100)
+//        outputStr = output.prefix(8).map(String.init).joined()
+//        XCTAssertEqual("73745418", outputStr)
+//
+//        input = day.parse("69317163492948606335995924319873")
+//        output = FFT.process(input: &input, phases: 100)
+//        outputStr = output.prefix(8).map(String.init).joined()
+//        XCTAssertEqual("52432133", outputStr)
+    }
+
+    func testPhaseOffsetIdea() {
+        var input = [1,2,3,4,5,6,7,8]
+        FFT.phase(input: &input, offset: 1)
+        print(input)
+        XCTAssertEqual(8, input[1])
+        print(input)
+        FFT.phase(input: &input, offset: 1)
+        XCTAssertEqual(4, input[1])
+        print(input)
+    }
+
+    func testPartTwo() {
+        let day = DaySixteen()
+        var output: String
+
+        output = day.partTwo(input: "03036732577212944063491565474664") as! String
+        XCTAssertEqual("84462026", output)
+
+        output = day.partTwo(input: "02935109699940807407585447034323") as! String
+        XCTAssertEqual("78725270", output)
+
+        output = day.partTwo(input: "03081770884921959731165446850517") as! String
+        XCTAssertEqual("53553731", output)
     }
 
 }
