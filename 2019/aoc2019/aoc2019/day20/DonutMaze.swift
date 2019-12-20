fe775aeb5c6633c7a7648bef23db9da62bd1d9e1
Shawn Veader <shawn@veader.org>
2019-12-20 16:26:37 -0500

    Day 20: ** (WIP)

--- 2019/aoc2019/aoc2019/day20/DonutMaze.swift
+++ 2019/aoc2019/aoc2019/day20/DonutMaze.swift
@@ -27,6 +27,30 @@ struct DonutMaze {
     /// Connections between portals. Stored by name with in/out coordinates
     let portalConnections: [String: [Coordinate]]
 
+    var portals: [Coordinate] {
+        map.compactMap { (coord, tile) -> Coordinate? in
+            guard case .portal(name: _) = tile else { return nil }
+            return coord
+        }
+    }
+    
+    var minX: Int {
+        map.keys.min(by: { $0.x < $1.x })?.x ?? 0
+    }
+
+    var maxX: Int {
+        map.keys.max(by: { $0.x < $1.x })?.x ?? 0
+    }
+
+    var minY: Int {
+        map.keys.min(by: { $0.y < $1.y })?.y ?? 0
+    }
+
+    var maxY: Int {
+        map.keys.max(by: { $0.y < $1.y })?.y ?? 0
+    }
+
+
     init(input: String) {
         var tmpStart: Coordinate? = nil
         var tmpGoal: Coordinate? = nil
@@ -104,26 +128,77 @@ struct DonutMaze {
         goal = tmpGoal
     }
 
+
+    // MARK: - Searching
+
+    struct SearchProgress {
+        // hash by recursion level, then by coordinate to distance
+        var distanceMap = [Int: [Coordinate: Int]]()
+
+        // hashed by portal name and recursion levels used on
+        var usedPortals = [String: [Int]]()
+
+        func distance(location: Coordinate, level: Int) -> Int? {
+            guard
+                let levelMap = distanceMap[level],
+                let distance = levelMap[location]
+                else { return nil }
+
+            return distance
+        }
+
+        mutating func record(distance: Int, location: Coordinate, level: Int) {
+            if var levelMap = distanceMap[level] {
+                levelMap[location] = distance
+                distanceMap[level] = levelMap // just in case
+            } else {
+                var newMap = [Coordinate: Int]()
+                newMap[location] = distance
+                distanceMap[level] = newMap
+            }
+        }
+
+        func wasUsed(portal: String, level: Int) -> Bool {
+            guard let levels = usedPortals[portal], levels.contains(level) else { return false }
+            return true
+        }
+
+        mutating func markUsed(level: Int, portal: String) {
+            usedPortals[portal] = (usedPortals[portal] ?? []) + [level]
+        }
+
+        mutating func removeUsedMark(level: Int, portal: String) {
+            usedPortals[portal] = (usedPortals[portal] ?? []).filter { $0 != level}
+        }
+    }
+
+
     /// Determine the shortest path from start to the end.
-    func shortestPath() -> Int {
-        var distanceMap = [Coordinate: Int]()
+    func shortestPath(recursive: Bool = false) -> Int {
+        var progress = SearchProgress()
         guard start != nil, goal != nil else { return Int.max }
 
-        return bfSearch(location: start!, distance: 0, progress: &distanceMap)
+        return bfSearch(location: start!, distance: 0, level: 0, progress: &progress, recursive: recursive)
     }
 
     /// Use BFS to find shortest path from this location
-    private func bfSearch(location: Coordinate, distance: Int, progress distanceMap: inout [Coordinate: Int]) -> Int {
-        if let prevDistance = distanceMap[location], prevDistance < distance {
+    private func bfSearch(location: Coordinate, distance: Int, level: Int, progress: inout SearchProgress, recursive: Bool) -> Int {
+        if let prevDistance = progress.distance(location: location, level: level), prevDistance < distance {
             return Int.max // we visited here before on a shorter route
         }
 
-        distanceMap[location] = distance
+        // record our distance
+        progress.record(distance: distance, location: location, level: level)
 
         // have we hit our goal?
         if goal == location {
-            print("Hit the goal \(distance)")
-            return distance
+            if level == 0 {
+                print("Hit the goal \(distance) - level \(level)")
+                return distance
+            } else {
+                print("Hit ZZ but at level \(level)")
+                return Int.max
+            }
         }
 
         // if we are on a portal, teleport to the other side...
@@ -141,8 +216,66 @@ struct DonutMaze {
                 }
                 return false
             }) {
-                print("Teleporting: \(name)")
-                return bfSearch(location: target, distance: distance, progress: &distanceMap)
+                var newLevel = level
+
+                var debugText = ""
+
+                if recursive {
+                    // TODO: we might have to care about level + coordinate
+
+
+                    // before we step into a portal, see if has been used on
+                    //  this level before. if so, we need to pop levels; otherwise push
+                    if progress.wasUsed(portal: name, level: level) {
+                        newLevel = max(0, level - 1) // used portal mean we are going down
+                        debugText = "Popping a level entering used \(name) on \(level) -> \(newLevel)"
+
+                        // clear mark as we go down
+                        progress.removeUsedMark(level: level, portal: name)
+                    } else {
+                        newLevel = level + 1 // unused portal means we we are going up
+                        debugText = "Pushing a level entering \(name) on \(level) -> \(newLevel)"
+
+                        // mark the next level up so we know how to get down
+                        progress.markUsed(level: newLevel, portal: name)
+                    }
+
+
+                    // TODO: problem here is we are getting negative levels
+
+                    /*
+                    // determine resursive
+                    if isInner(portal: location) && isOuter(portal: otherSide) {
+                        // popping a level
+                        newLevel -= 1
+                        print("Teleporting: \(name) - popping level from \(level) to \(newLevel)")
+                    } else if isInner(portal: otherSide) && isOuter(portal: location) {
+                        // pushing on a level
+                        newLevel += 1
+                        print("Teleporting: \(name) - pushing level from \(level) to \(newLevel)")
+                    } else {
+                        print("Level switch unknown. Portal \(name)... \(location) \(otherSide)")
+                        print("X: \(minX)..\(maxX), Y: \(minY)..\(maxY)")
+                    }
+                     */
+
+                    // mark this portal as closed on both sides
+//                    progress.markUsed(level: level, portal: name)
+//                    progress.markUsed(level: newLevel, portal: name)
+//                    print(progress.usedPortals)
+                }
+
+                // double check what is on the other side to prevent flopping
+                if let portalDistance = progress.distance(location: target, level: newLevel), portalDistance < distance {
+                    print("\tPeered through the portal \(name) on \(level) looking to level \(newLevel); someone has been there quicker before...")
+                    return Int.max
+                }
+
+                if !debugText.isEmpty {
+                    print(debugText)
+                }
+
+                return bfSearch(location: target, distance: distance, level: newLevel, progress: &progress, recursive: recursive)
             } else {
                 print("Unable to teleport to \(name)")
             }
@@ -155,7 +288,15 @@ struct DonutMaze {
                 switch tile {
                 case .wall:
                     return nil
-                default:
+//                case .portal(name: let name):
+//                    if progress.wasUsed(portal: name, level: level) {
+//                        print("Hit used portal \(name)")
+//                        return nil
+//                    } else {
+//                        return nextLocation
+//                    }
+                default: // empty spaces and portals
+                    guard nextLocation != start else { return nil }
                     return nextLocation
                 }
             }
@@ -164,9 +305,21 @@ struct DonutMaze {
         }
 
         let shortestPaths = nextSteps.map {
-            bfSearch(location: $0, distance: distance + 1, progress: &distanceMap)
+            bfSearch(location: $0, distance: distance + 1, level: level, progress: &progress, recursive: recursive)
         }
 
         return shortestPaths.min() ?? Int.max
+    }
+
+
+    // MARK: - Helpers
+    func isInner(portal: Coordinate) -> Bool {
+        portal.x != minX && portal.y != minY &&
+        portal.x != maxX && portal.y != maxY
+    }
+
+    func isOuter(portal: Coordinate) -> Bool {
+        portal.x == minX || portal.y == minY ||
+        portal.x == maxX || portal.y == maxY
     }
 }
--- 2019/aoc2019/aoc2019Tests/DayTwentyTests.swift
+++ 2019/aoc2019/aoc2019Tests/DayTwentyTests.swift
@@ -32,6 +32,37 @@ class DayTwentyTests: XCTestCase {
         XCTAssertEqual(58, distance2)
     }
 
+    func testInnerOuter() {
+        let maze = DonutMaze(input: testInput1)
+
+        // FG
+        XCTAssert(maze.isInner(portal: Coordinate(x: 11, y: 11)))
+        XCTAssert(maze.isOuter(portal: Coordinate(x: 1, y: 15)))
+
+        // DE
+        XCTAssert(maze.isInner(portal: Coordinate(x: 7, y: 10)))
+        XCTAssert(maze.isOuter(portal: Coordinate(x: 1, y: 13)))
+
+        // BC
+        XCTAssert(maze.isInner(portal: Coordinate(x: 9, y: 7)))
+        XCTAssert(maze.isOuter(portal: Coordinate(x: 1, y: 8)))
+
+        let maze2 = DonutMaze(input: testInput2)
+        print(maze2.portalConnections)
+
+        // VT
+        XCTAssert(maze2.isInner(portal: Coordinate(x: 25, y: 23)))
+        XCTAssert(maze2.isOuter(portal: Coordinate(x: 33, y: 11)))
+    }
+
+    func testRecursivePathFinding() {
+        let maze = DonutMaze(input: testInput3)
+        print(maze.portalConnections)
+        print(maze.portals)
+        let distance = maze.shortestPath(recursive: true)
+        XCTAssertEqual(396, distance)
+    }
+
     func testPartOneAnswer() {
         let day = DayTwenty()
         let answer = day.run(part: 1) as! Int
@@ -105,5 +136,45 @@ class DayTwentyTests: XCTestCase {
                        #########.###.###.#############
                                 B   J   C
                                 U   P   P
+                     """
+
+    let testInput3 = """
+                                  Z L X W       C
+                                  Z P Q B       K
+                       ###########.#.#.#.#######.###############
+                       #...#.......#.#.......#.#.......#.#.#...#
+                       ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###
+                       #.#...#.#.#...#.#.#...#...#...#.#.......#
+                       #.###.#######.###.###.#.###.###.#.#######
+                       #...#.......#.#...#...#.............#...#
+                       #.#########.#######.#.#######.#######.###
+                       #...#.#    F       R I       Z    #.#.#.#
+                       #.###.#    D       E C       H    #.#.#.#
+                       #.#...#                           #...#.#
+                       #.###.#                           #.###.#
+                       #.#....OA                       WB..#.#..ZH
+                       #.###.#                           #.#.#.#
+                     CJ......#                           #.....#
+                       #######                           #######
+                       #.#....CK                         #......IC
+                       #.###.#                           #.###.#
+                       #.....#                           #...#.#
+                       ###.###                           #.#.#.#
+                     XF....#.#                         RF..#.#.#
+                       #####.#                           #######
+                       #......CJ                       NM..#...#
+                       ###.#.#                           #.###.#
+                     RE....#.#                           #......RF
+                       ###.###        X   X       L      #.#.#.#
+                       #.....#        F   Q       P      #.#.#.#
+                       ###.###########.###.#######.#########.###
+                       #.....#...#.....#.......#...#.....#.#...#
+                       #####.#.###.#######.#######.###.###.#.#.#
+                       #.......#.......#.#.#.#.#...#...#...#.#.#
+                       #####.###.#####.#.#.#.#.###.###.#.###.###
+                       #.......#.....#.#...#...............#...#
+                       #############.#.#.###.###################
+                                    A O F   N
+                                    A A D   M
                      """
 }
