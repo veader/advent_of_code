7865ad29ddb73fef2f42ddb81da532804b4cb347
Shawn Veader <shawn@veader.org>
2024-12-03 09:10:44 -0500

    2024: Day 3 **

--- AdventOfCode/AdventOfCode/2024/Day03/Day3_2024.swift
+++ AdventOfCode/AdventOfCode/2024/Day03/Day3_2024.swift
@@ -12,16 +12,24 @@ struct Day3_2024: AdventDay {
     var year = 2024
     var dayNumber = 3
     var dayTitle = "Mull It Over"
-    var stars = 1
+    var stars = 2
 
+    /// Matches `mul(1,3)`, `mul(1,23)`, `mul(12,345)` (with 1-3 digit numbers, no spaces)
     let mulInstructionRegex: Regex = /mul\((\d{1,3}),(\d{1,3})\)/
+
+    /// Matches `do()`
+    let doInstructionRegex: Regex = /do\(\)/
+
+    /// Matches `don't()`
+    let dontInstructionRegex: Regex = /don\'t\(\)/
 
     typealias MultiplyInstruction = (x: Int, y: Int)
 
-    func parse(_ input: String?) -> [[MultiplyInstruction]] {
-        (input ?? "").split(separator: "\n").map(String.init).map { parse(line: $0) }
+    func parse(_ input: String?) -> [String] {
+        (input ?? "").split(separator: "\n").map(String.init)
     }
 
+    /// Parse the given line just looking for `mul` instructions.
     func parse(line: String) -> [MultiplyInstruction] {
         let matches = line.matches(of: mulInstructionRegex)
         guard matches.count > 0 else { return [] }
@@ -31,16 +39,52 @@ struct Day3_2024: AdventDay {
         }
     }
 
+    /// Parse the given line looking for all instructions and enable/disable `mul`
+    /// instructions based on previous `do` and `don't` instructions.
+    ///
+    /// - Note: We need all lines because the enabled state should be tracked across
+    /// line boundaries.
+    func parseTrackingEnabledState(lines: [String]) -> [MultiplyInstruction] {
+        let allRegex = ChoiceOf {
+            mulInstructionRegex
+            doInstructionRegex
+            dontInstructionRegex
+        }
+
+        var instructions = [MultiplyInstruction]()
+        var enabled = true // default to enabled
+
+        for line in lines {
+            let matches = line.matches(of: allRegex)
+            guard matches.count > 0 else { return [] }
+
+            for fullMatch in matches {
+                if let match = fullMatch.0.firstMatch(of: mulInstructionRegex) {
+                    guard enabled else { continue } // skip instructions when disabled
+                    guard let x = Int(match.1), let y = Int(match.2) else { continue }
+                    instructions.append(MultiplyInstruction(x: x, y: y))
+                } else if let _ = fullMatch.0.firstMatch(of: doInstructionRegex) {
+                    enabled = true
+                } else if let _ = fullMatch.0.firstMatch(of: dontInstructionRegex) {
+                    enabled = false
+                }
+            }
+        }
+
+        return instructions
+    }
+
     func execute(instructions: [MultiplyInstruction]) -> Int {
         instructions.map { $0.x * $0.y }.reduce(0, +)
     }
 
     func partOne(input: String?) -> Any {
-        let instructionSets = parse(input)
+        let instructionSets = parse(input).map { parse(line: $0) }
         return instructionSets.map { execute(instructions: $0) }.reduce(0, +)
     }
 
     func partTwo(input: String?) -> Any {
-        return 0
+        let instructions = parseTrackingEnabledState(lines: parse(input))
+        return execute(instructions: instructions)
     }
 }
--- AdventOfCode/AdventOfCodeTests/2024/Day3_2024Tests.swift
+++ AdventOfCode/AdventOfCodeTests/2024/Day3_2024Tests.swift
@@ -9,12 +9,26 @@ import Testing
 
 struct Day3_2024Tests {
 
-    let sampleData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    let sampleData  = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    let sampleData2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
     let day = Day3_2024()
 
     @Test func testParsingLine() async throws {
         let instructions = day.parse(line: sampleData)
         #expect(instructions.count == 4)
+
+        let first = try #require(instructions.first)
+        #expect(first.x == 2)
+        #expect(first.y == 4)
+
+        let last = try #require(instructions.last)
+        #expect(last.x == 8)
+        #expect(last.y == 5)
+    }
+
+    @Test func testParsingLineWithEnabledTracking() async throws {
+        let instructions = day.parseTrackingEnabledState(lines: [sampleData2])
+        #expect(instructions.count == 2)
 
         let first = try #require(instructions.first)
         #expect(first.x == 2)
@@ -31,6 +45,12 @@ struct Day3_2024Tests {
         #expect(answer == 161)
     }
 
+    @Test func testExecutingInstructionsWithEnabledTracking() async throws {
+        let instructions = day.parseTrackingEnabledState(lines: [sampleData2])
+        let answer = day.execute(instructions: instructions)
+        #expect(answer == 48)
+    }
+
     @Test func testPartOneWithSampleData() async throws {
         let answer = try #require(day.partOne(input: sampleData) as? Int)
         #expect(answer == 161)
@@ -38,6 +58,17 @@ struct Day3_2024Tests {
 
     @Test func testPartOne() async throws {
         let answer = try await #require(day.run(part: 1) as? Int)
-        #expect(answer == 170778545)
+        #expect(answer == 170_778_545)
     }
+
+    @Test func testPartTwoWithSampleData() async throws {
+        let answer = try #require(day.partTwo(input: sampleData2) as? Int)
+        #expect(answer == 48)
+    }
+
+    @Test func testPartTwo() async throws {
+        let answer = try await #require(day.run(part: 2) as? Int)
+        #expect(answer == 82_868_252) // too high
+    }
+
 }
